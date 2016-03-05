01483a1b6aee71788eddf13086c77019185fa26e
virusdefender <qduliyang@outlook.com>
2016-03-05 16:57:57 +0800

    修复正则中不正确的转义

--- robots/pat.py
+++ robots/pat.py
@@ -15,8 +15,9 @@ class PATRobot(Robot):
         return re.compile(regex).match(url) is not None
 
     def _get_token(self):
-        r = self.get("http://www.patest.cn/contests", cookies=self.cookies)
-        self.token = re.compile(r"<meta content=\"(.*)\" name=\"csrf-token\" />").findall(r.text)[0]
+        r = self.get("https://www.patest.cn/contests", cookies=self.cookies)
+        self.check_status_code(r)
+        self.token = re.compile(r'<meta content="(.*)" name="csrf-token" />').findall(r.text)[0]
 
     def login(self, username, password):
         r = self.post("http://www.patest.cn/users/sign_in",
@@ -43,14 +44,16 @@ class PATRobot(Robot):
     def get_problem(self, url):
         if not self.check_url(url):
             raise RequestFailed("Invalid PAT url")
-        problem_id = "pat-" + "-".join(re.compile(r"^http://www.patest.cn/contests/pat-(a|b|t)-practise/(\d{4})$").findall(url)[0])
-        regex = {"title": r"<div id=\"body\" class=\"span-22 last\">\s*<h1>(.*)</h1>",
+        problem_id = "pat-" + "-".join(re.compile(r"^https://www.patest.cn/contests/pat-(a|b|t)-practise/(\d{4})$").findall(url)[0])
+        regex = {"title": r'<div id="body" class="span-22 last">\s*<h1>(.*)</h1>',
                  "time_limit": r"<div class='key'>\s*时间限制\s*</div>\s*<div class='value'>\s*(\d+) ms",
                  "memory_limit": r"<div class='key'>\s*内存限制\s*</div>\s*<div class='value'>\s*(\d+) kB",
                  "description": r"<div id='problemContent'>([\s\S]*?)<b>\s*(?:Input|Input Specification:|输入格式：)\s*</b",
                  "input_description": r"<b>\s*(?:Input|Input Specification:|输入格式：)\s*</b>([\s\S]*?)<b>\s*(?:Output|Output Specification:|输出格式：)\s*</b>",
                  "output_description": r"<b>\s*(?:Output|Output Specification:|输出格式：)\s*</b>([\s\S]*?)<b>\s*(?:Sample Input|输入样例).*</b>",
-                 "samples": r"<b>\s*(?:Sample Input|输入样例)\s*(?P<t_id>\d?).?</b>\s*<pre>([\s\S]*?)</pre>\s+<b>(?:Sample Output|输出样例)\s?(?P=t_id).?</b>\s*<pre>([\s\S]*?)</pre>"}
+                 "samples": r"<b>\s*(?:Sample Input|输入样例)\s*(?P<t_id>\d?).?</b>\s*<pre>([\s\S]*?)</pre>\s+<b>(?:Sample Output|输出样例)\s?(?P=t_id).?</b>\s*<pre>([\s\S]*?)</pre>",
+                 "submit_url": r'<form accept-charset="UTF-8" action="([\s\S]*?)" method="post">'}
+
         data = self._regex_page(url, regex)
         data["id"] = problem_id
         return data
