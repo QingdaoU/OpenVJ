1ed0cce64be6a76f6ea2486b4cebb783b60c0bd6
ltwy <1043218708@qq.com>
2016-03-05 21:23:20 +0800

    完成题目扒取和提交接口

---
+++ robots/tests.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+import unittest
+from robots.zoj import ZOJRobot
+from robots.pat import PATRobot
+
+class ZOJRobotTest(unittest.TestCase):
+
+    def setUp(self):
+        self.tclass = ZOJRobot()
+
+    def tearDown(self):
+        pass
+
+    # def test_check_url(self):
+    #     self.assertEqual(self.tclass.check_url(r"http://acm.zju.edu.cn/onlinejudge/showProblem.do?problemCode=12234"), False)
+    #     self.assertEqual(self.tclass.check_url(r"http://acm.zju.edu.cn/onlinejudge/showProblem.do?problemCode=12"), False)
+    #     self.assertEqual(self.tclass.check_url(r"http://acm.zju.edu.cn/onlinejudge/showProblem.do?problemCode=1234"), True)
+    #     self.assertEqual(self.tclass.check_url(r"http://acm.zju.edu.cn/onlinejudge/showProblem.do?problemCode=1223"), True)
+    #
+    # def test_login(self):
+    #     self.assertEquals(self.tclass.login("ltwy", "jiangxuelei"), None)
+    #
+    # def test_is_logged_in(self):
+    #     self.tclass.login("ltwy", "jiangxuelei")
+    #     self.assertEquals(self.tclass.is_logged_in, True)
+    #     self.tclass.logout()
+    #     self.assertEquals(self.tclass.is_logged_in, False)
+
+    def test_get_problem(self):
+        self.tclass.login("ltwy", "jiangxuelei")
+        self.assertEquals(self.tclass.get_problem(r"http://acm.zju.edu.cn/onlinejudge/showProblem.do?problemCode=3705"), None)
+
+
+
--- robots/zoj.py
+++ robots/zoj.py
@@ -9,7 +9,7 @@ from .utils import Language
 class ZOJRobot(Robot):
 
     def check_url(self, url):
-        regex = r"^http://acm.zju.edu.cn/onlinejudge/showProblem.do\?problemCode=\d{4}$"
+        regex = r"^http://acm.zju.edu.cn/onlinejudge/showProblem.do\?problemCode=(\d{4})$"
         return re.compile(regex).match(url) is not None
 
     def login(self, username, password):
@@ -40,20 +40,19 @@ class ZOJRobot(Robot):
     def get_problem(self, url):
         if not self.check_url(url):
             raise RequestFailed("Invalid ZOJ URL!")
-        regex = r"^http://acm.zju.edu.cn/onlinejudge/showProblem.do\?problemCode=\d{4}$"
-        problem_id = re.compile(regex).findall(url)[0][0]
+        regex = r"^http://acm.zju.edu.cn/onlinejudge/showProblem.do\?problemCode=(\d{4})$"
+        problem_id = re.compile(regex).findall(url)[0]
         regex = {
-                "title": r"<center><span class=\"bigProblemTitle\">.*</span></center>",
-                "time_Limit": r"<font color=\"green\">Time Limit: </font> \d+ Seconds",
-                "memory_limit": r"<font color=\"green\">Memory Limit: </font> 32768 KB",
-                "description": r"",
-                "input_description": r"",
-                "output_description": r"",
-                "samples": r""
+                "title": r"<center><span\s*class=\"bigProblemTitle\">(.*)</span></center>",
+                "time_Limit": r"<font\s*color=\"green\">Time\s*Limit:\s*</font>\s*(\d+)\s*Seconds",
+                "memory_limit": r"<font\s*color=\"green\">Memory\s*Limit:\s*</font>\s*(\d+)\s*KB",
+                "description": r"</center>\s*<hr>\s*([\s\S]*?)\s*(?:<b>|<h4>)Input",
+                "input_description": r"(?:<h4>|<b>|<strong>)Input(?:</h4>|</b>|</strong>)\s*([\s\S]*?)\s*(?:<h4>|<b>|<strong>)Output(?:</h4>|</b>|</strong>)",
+                "output_description": r"(?:<h4>|<b>|<strong>)Output(?:</h4>|</b>|</strong>)\s*([\s\S]*?)\s*(?:<h4>|<b>|<strong>)Sample Input",
+                "samples": r"(?:<h4>|<b>|<strong>)Sample Input(?:</h4>|</b>|</strong>)\s*<pre>([\s\S]*?)</pre>\s*(?:<h4>|<strong>|<b>)Sample Output(?:</b>|</strong>|</h4>)\s*<pre>([\s\S]*?)</pre>"
         }
         data = self._regex_page(url, regex)
-        data.id = problem_id
-        return data
+        data["id"]= problem_id
 
     def _regex_page(self, url, regex):
         r = self.get(url)
@@ -62,53 +61,30 @@ class ZOJRobot(Robot):
         for k,v in regex.items():
             items = re.compile(v).findall(r.text)
             if not items:
+                print(k)
                 raise RegexError("NO such data!")
             if k != "samples":
                 data[k] = self._clean_html(items[0])
             else :
-                tmp = []
-                for item in items:
-                    tmp.append({"input": self._clean_html(item[0]), "output": self._clean_html((item[1]))})
-                data[k] = tmp
+                data[k] = {"items[0][0]": items[0][1]}
         return data
 
-    def _clean_html(self, text):
-        return self._decode_html(re.compile(r"<h4>|</h4>|<p>|</p>|<b>|</b>|\r|\n|<span>|</span>").sub("", text));
-
-    def submit(self, url, language, code):
+    def submit(self, submit_url, language, code, orginal_id):
+        if(self.is_logged_in() == False):
+            raise AuthFailed("Not login!")
         if language == Language.C:
             compiler_id = "1"
         elif language == Language.CPP:
             compiler_id = "2"
         else:
             compiler_id = "4"
-        regex = r"^http://acm.zju.edu.cn/onlinejudge/showProblem.do\?problemCode=\d{4}$"
-        problem_id = re.compile(regex).findall(url)[0][0]
-        r = self.post(url, data={"problemId": problem_id, "languageId": compiler_id, "source": code},
+        r = self.post(submit_url, data={"problemId": orginal_id, "languageId": compiler_id, "source": code},
                       cookies=self.cookies,
                       headers={"Referer": "http://acm.zju.edu.cn/",
                                "Content-Type": "application/x-www-form-urlencoded"})
         if r.status_code != 200:
-            raise SubmitProblemFailed("Failed to submit problem, url: %s, status code: %d" % (url, r.status_code))
-        return str(re.compile(r"<p>Your source has been submitted. The submission id is <font color='red'>\d+</font>").findall(r.text)[0][0])
-
-    def _decode_html(self, text):
-        """
-        html实体编码的解码
-        比如 &gt -> >  &lt -> <
-        """
-        return html.unescape(text)
+            raise SubmitProblemFailed("Failed to submit problem, url: %s, status code: %d" % (submit_url, r.status_code))
+        return str(re.compile(r"<p>Your source has been submitted. The submission id is <font color='red'>(\d+)</font>").findall(r.text)[0])
 
     def get_result(self, submission_id):
         pass
-    def check_status_code(self, response, status_code=200):
-        """
-        检查响应是否是指定的status code 否则引发异常
-        :param response:
-        :param status_code:
-        :return:
-        """
-        if response.status_code != status_code:
-            raise RequestFailed("Invalid status code [%d] when fetching url [%s], expected %d" %
-                                (response.status_code, response.url, status_code))
-
