0898902c105d542564d1ae4a5e53b0b4ae69ff0d
virusdefender <qduliyang@outlook.com>
2016-03-06 18:41:47 +0800

    删除部分检查登录状态的函数并增加save方法

--- robots/zoj.py
+++ robots/zoj.py
@@ -7,6 +7,8 @@ from .utils import Language, Result
 
 
 class ZOJRobot(Robot):
+    def save(self):
+        return {"cookies": self.cookies}
 
     def check_url(self, url):
         regex = r"^http://acm.zju.edu.cn/onlinejudge/showProblem.do\?problemCode=(\d{4})$"
@@ -29,9 +31,6 @@ class ZOJRobot(Robot):
             raise AuthFailed("Failed to login ZOJ!")
         self.cookies = dict(r.cookies)
 
-    def logout(self):
-        self.cookies = None
-
     @property
     def is_logged_in(self):
         r = self.get("http://acm.zju.edu.cn/onlinejudge/editProfile.do", cookies = self.cookies)
@@ -40,8 +39,6 @@ class ZOJRobot(Robot):
     def get_problem(self, url):
         if not self.check_url(url):
             raise RequestFailed("Invalid ZOJ URL!")
-        regex = r"^http://acm.zju.edu.cn/onlinejudge/showProblem.do\?problemCode=(\d{4})$"
-        problem_id = re.compile(regex).findall(url)[0]
         regex = {
                 "title": r"<center><span\s*class=\"bigProblemTitle\">(.*)</span></center>",
                 "time_Limit": r"<font\s*color=\"green\">Time\s*Limit:\s*</font>\s*(\d+)\s*(?:Seconds|Second)",
@@ -53,43 +50,41 @@ class ZOJRobot(Robot):
                 "hint": r'(?:<h4>|<b>|<strong>)Hint(?:</h4>|</b>|</strong>)\s*<p>[\s\S]*<hr>'
         }
         data = self._regex_page(url, regex)
-        data["id"]= problem_id
+        data["id"] = re.compile(r"^http://acm.zju.edu.cn/onlinejudge/showProblem.do\?problemCode=(\d{4})$").findall(url)[0]
+        return data
 
     def _regex_page(self, url, regex):
         r = self.get(url)
         self.check_status_code(r)
         data = {}
-        for k,v in regex.items():
+        for k, v in regex.items():
             items = re.compile(v).findall(r.text)
             if not items:
                 raise RegexError("NO such data!")
             if k != "samples":
                 data[k] = self._clean_html(items[0])
-            else :
+            else:
                 data[k] = {items[0][0]: items[0][1]}
         return data
 
     def submit(self, submit_url, language, code, origin_id):
-        if(self.is_logged_in == False):
-            raise AuthFailed("Not login!")
         if language == Language.C:
             compiler_id = "1"
         elif language == Language.CPP:
             compiler_id = "2"
         else:
             compiler_id = "4"
-        r = self.post(submit_url, data={"problemId": str(int(origin_id)-1000), "languageId": compiler_id, "source": code},
+        r = self.post(submit_url,
+                      data={"problemId": str(int(origin_id) - 1000), "languageId": compiler_id, "source": code},
                       cookies=self.cookies,
                       headers={"Referer": "http://acm.zju.edu.cn/",
                                "Content-Type": "application/x-www-form-urlencoded"})
         if r.status_code != 200:
             raise SubmitProblemFailed("Failed to submit problem, url: %s, status code: %d" % (submit_url, r.status_code))
-        return str(re.compile(r"<p>Your source has been submitted. The submission id is <font color='red'>(\d+)</font>").findall(r.text)[0])
+        return re.compile(r"<p>Your source has been submitted. The submission id is <font color='red'>(\d+)</font>").findall(r.text)[0]
 
     def get_result(self, submission_id, username):
-        if(self.is_logged_in == False):
-            raise AuthFailed("Not login!")
-        url = r"http://acm.zju.edu.cn/onlinejudge/showRuns.do?contestId=1&search=true&firstId=-1&lastId=-1&problemCode=&handle=&idStart="+submission_id+r"&idEnd="+submission_id
+        url = r"http://acm.zju.edu.cn/onlinejudge/showRuns.do?contestId=1&search=true&firstId=-1&lastId=-1&problemCode=&handle=&idStart=" + submission_id+r"&idEnd="+submission_id
         r = self.get(url, headers={r"Referer": r"http://acm.zju.edu.cn/"}, cookies=self.cookies)
         res = {}
         val = {
@@ -126,4 +121,4 @@ class ZOJRobot(Robot):
             res["cpu_time"] = re.compile(regex["cpu_time"]).findall(r.text)[0]
             res["memory"] = re.compile(regex["memory"]).findall(r.text)[0]
             res["info"] = None
-        return res;
+        return res
