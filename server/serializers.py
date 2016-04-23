2948d0c9e017781ad7913192d701548a73f78b9d
virusdefender <qduliyang@outlook.com>
2016-04-23 15:09:56 +0800

    增加处理submission的serializer

--- server/serializers.py
+++ server/serializers.py
@@ -4,7 +4,7 @@ import json
 from rest_framework import serializers
 
 from robots.utils import Language
-from .models import Problem
+from .models import Problem, Submission
 
 
 class JSONField(serializers.Field):
@@ -23,3 +23,11 @@ class CreateSubmissionSerializer(serializers.Serializer):
     problem_id = serializers.CharField(max_length=32)
     language = serializers.ChoiceField(choices=[Language.C, Language.CPP, Language.Java])
     code = serializers.CharField(max_length=10000)
+
+
+class SubmissionSerializer(serializers.ModelSerializer):
+    info = JSONField()
+
+    class Meta:
+        model = Submission
+        exclude = ["submit_task_id", "task_id", "robot_user", "origin_submission_id", "api_key"]
